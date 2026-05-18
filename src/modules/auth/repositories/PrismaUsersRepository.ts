b07423a1c5adddb0e1ac67d0eef2fdf7fa9c9973
Reuel Ferreira <ferreirareuel3@gmail.com>
2026-05-18 17:12:14 -0300

    feat: implementando respositories de auth. Fixes ENG-3.

---
+++ src/modules/auth/repositories/PrismaUsersRepository.ts
@@ -0,0 +1,25 @@
+import { PrismaClient, Prisma, User } from "@prisma/client"
+import { IUserRepository } from './IUserRepository'
+
+const prisma = new PrismaClient()
+
+export class PrismaUsersRepository implements IUserRepository {
+
+    async create(data: Prisma.UserUncheckedCreateInput): Promise<User> {
+        const user = await prisma.user.create({
+            data
+        })
+
+        return user
+    }
+
+    async findByEmail(email: string): Promise<User | null> {
+        const user = await prisma.user.findUnique({
+            where: {
+                email
+            }
+        })
+
+        return user
+    }
+}
